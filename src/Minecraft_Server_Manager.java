4a9b12a73af971fa14cd2d28873c820c414da55f
Robert Coulter <RobertCoulter95@gmail.com>
2019-07-02 15:10:30 -0400

    Added PSVM to Minecraft_Server_Manager.java.

--- src/Minecraft_Server_Manager.java
+++ src/Minecraft_Server_Manager.java
@@ -1,10 +1,8 @@
 import java.io.File;
 
 public class Minecraft_Server_Manager {
-    String World;
-    File properties;
 
     public static void main(String[] args) {
-
+        //Test text.
     }
 }
