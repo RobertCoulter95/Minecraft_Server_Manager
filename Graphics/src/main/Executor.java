838d79ad2ccc6baa031d3e4487a28032ba4b812e
Robert Coulter <RobertCoulter95@gmail.com>
2019-07-04 18:21:20 -0400

    Removed Server module and put contents into graphics module.

--- Graphics/src/main/Executor.java
+++ Graphics/src/main/Executor.java
@@ -2,9 +2,41 @@ package main;
 
 import Main_GUI.Main;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+
+
 public class Executor {
-    public static void main(String[] args) {
+    private static String JavaLocation = System.getProperty("java.home") + "/bin/java";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         //Calls module Graphics, to open graphics.
-        Main.main("Start");
+
+
+        Thread ThreadedUI = new Thread(){
+            public void run(){
+                Main.main("Start");
+            }
+        };
+
+        Thread ThreadedServer = new Thread(){
+            public void run(){
+                ProcessBuilder pb = new ProcessBuilder(JavaLocation, "-jar", "server.jar");
+                try {
+                    Process p = pb.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        ThreadedServer.start();
+        ThreadedUI.start();
+
+        //System.out.println(System.getProperty("java.home"));
+
     }
 }
--- src/Minecraft_Server_Manager.java
+++
@@ -1,8 +0,0 @@
-import java.io.File;
-
-public class Minecraft_Server_Manager {
-
-    public static void main(String[] args) {
-        //Test text.
-    }
-}
